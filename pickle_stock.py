6b7d124630e543b64df045c50c1d4f21c25bf08a
Robby Tong <robbytong@Robbys-MacBook-Pro.local>
2017-03-25 14:39:52 -0700

    it graphs

--- pickle_stock.py
+++ pickle_stock.py
@@ -1,9 +1,13 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 from yahoo_finance import Share
 from pprint import pprint
+import numpy as np
 import datetime
 import pickle
 import time
 import sys
+import pdb
 
 HEADER = '\033[95m'
 BLUE = '\033[94m'
@@ -16,14 +20,7 @@ BOLD = '\033[1m'
 DIM = '\033[2m'
 UNDERLINE = '\033[4m'
 
-name = sys.argv[0]
-args = sys.argv[1:]
-
-if len(args) > 0:
-	stock_name = args[0]
-	delta = 7
-	if len(args) > 1:
-		delta = int(args[1])
+def pickle_stock(stock_name, delta):
 	stop_date = time.strftime('%Y-%m-%d')
 	convert = datetime.datetime.strptime(stop_date, '%Y-%m-%d') - datetime.timedelta(delta)
 	start_date = datetime.datetime.strftime(convert, '%Y-%m-%d')
@@ -37,9 +34,74 @@ if len(args) > 0:
 		outpu = open(filename, 'wb')
 		pickle.dump(histo, outpu)
 		outpu.close()
+		return True
 	except Exception as e:
 		print e
 		print '%sError:%s %s does not exist.'%(RED+UNDERLINE+BOLD, ENDC, stock_name)
+		return False
 
-else:
-	print "%sUsage:\n%s stock_sym [another_stock_sym [another_stock_sym ...]]"%(RED+UNDERLINE,ENDC+BLUE+BOLD+'python '+name+ENDC)
+def plot_stock(filename):
+	try:
+		fobj = open(filename, 'r')
+		data = pickle.load(fobj)
+		close_prices = []
+		open_prices = []
+		adj_close = []
+		volumes = []
+		dates = []
+		high = []
+		low = []
+		for day in data:
+			close_prices.append(float(day['Close']))
+			open_prices.append(float(day['Open']))
+			adj_close.append(float(day['Adj_Close']))
+			volumes.append(int(day['Volume']))
+			dates.append(datetime.datetime.strptime(day['Date'], '%Y-%m-%d'))
+			high.append(float(day['High']))
+			low.append(float(day['Low']))
+
+		ax = plt.gca()
+		years = mdates.YearLocator()   # every year
+		months = mdates.MonthLocator()  # every month
+		yearsFmt = mdates.DateFormatter('%m-%d\n%Y')
+
+		# format the ticks
+		ax.xaxis.set_major_locator(months)
+		ax.xaxis.set_major_formatter(yearsFmt)
+		ax.xaxis.set_minor_locator(months)
+
+		plt.subplot(211)
+		plt.plot(dates, close_prices)
+		plt.subplot(212)
+		plt.plot(dates, volumes, '.')
+		plt.show()
+
+		return True
+	except Exception as e:
+		print e
+		return False
+
+def usage():
+	name = sys.argv[0]
+	print UNDERLINE+CYAN+'Usage:'+ENDC+' python ' + name + ' ' + UNDERLINE + CYAN + 'stock symbol' + ENDC
+
+if __name__ == '__main__':
+	args = sys.argv[1:]
+	name = args[0]
+
+	if len(args) < 1:
+		print RED+UNDERLINE+BOLD+'Error:'+ENDC+' Not enough arguments'
+		usage()
+		sys.exit(-1)
+
+	downloaded = plot_stock(name)
+
+	if not downloaded:
+		if len(args) > 0:
+			stock_name = name
+			delta = 7
+			if len(args) > 1:
+				delta = int(args[1])
+			ok = pickle_stock(stock_name, delta)
+			if not ok:
+				usage()
